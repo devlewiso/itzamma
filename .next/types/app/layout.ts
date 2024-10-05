93d990532a7f0d3bcd2160c8862a91855d7eba6b
Iran Trinidad <devlewiso@gmail.com>
2024-10-05 01:05:29 -0600

    analytics added

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\devle\OneDrive\Documents\Build\reactjs\kaax\app\layout.tsx
+// File: /mnt/c/Users/devle/OneDrive/Escritorio/itzamma/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\devle\OneDrive\Documents\Build\reactjs\kaax\app\page.tsx
+// File: /mnt/c/Users/devle/OneDrive/Escritorio/itzamma/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
